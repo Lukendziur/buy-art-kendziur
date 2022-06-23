d389c6412776fce3fb65b9c92e592f1263c8aa97
Luken <lukendziur@hotmail.com>
2022-06-22 22:18:41 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import './App.css';
 import NavBar from './components/NavBar'
+// import ItemListContainer from './components/ItemListContainer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 function App() {
   return (
-   <NavBar/>
+  <>
+  <NavBar/>
+
+  </>  
   );
 }
 
