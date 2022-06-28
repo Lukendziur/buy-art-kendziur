1b2fe4ec5ef3bbcf020c8a23e489d33fd27a9ff9
Luken <lukendziur@hotmail.com>
2022-06-28 11:59:44 -0300

    update challenge 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import './App.css';
 import NavBar from './components/NavBar'
-// import ItemListContainer from './components/ItemListContainer'
+import ItemListContainer from './components/ItemListContainer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
@@ -9,8 +9,8 @@ function App() {
   return (
   <>
   <NavBar/>
-
-  </>  
+  <ItemListContainer greeting='hola desde prop'/>
+  </>
   );
 }
 
